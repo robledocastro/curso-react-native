8eeb56bdf6537d72119477235aa84d6d9567eead
Robledo Castro <bedocastro@gmail.com>
2025-03-31 11:00:21 -0300

    Implementando feed do Instagram usando FlatList

--- App.js
+++ App.js
@@ -1,94 +1,98 @@
 import React, { Component } from 'react';
-import { View, Text, TextInput, StyleSheet, Switch, Button, Alert} from 'react-native';
+import { 
+  View, 
+  Text, 
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  FlatList
+ } from 'react-native';
 
-import { Picker } from '@react-native-picker/picker';
-import Slider from '@react-native-community/slider';
+import Lista from './android/app/src/Lista';
 
 class App extends Component{
 
   constructor(props){
     super(props);
     this.state = {
-      nome: '',
-      Idade: '',
-      sexo: "aisuhsaiuh",
-      sexos: [
-        {key: 1, sexoNome: 'Masculino'},
-        {key: 2, sexoNome: 'Feminino'}
-      ],
-      limite: 1000,
-      isEstudante: false
+      feed: [
+        {
+          id: '1', 
+          nome: 'Lucas Silva', 
+          descricao: 'Mais um dia de muitos bugs :)', 
+          imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil1.png', 
+          imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto1.png',  
+          likeada: false, 
+          likers: 0
+         },
+        {
+          id: '2', 
+          nome: 'Matheus', 
+          descricao: 'Isso sim é ser raiz!!!!!', 
+          imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil2.png', 
+          imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto2.png', 
+          likeada: false, 
+          likers: 0
+        },
+        {
+          id: '3', 
+          nome: 'Jose Augusto', 
+          descricao: 'Bora trabalhar Haha', 
+          imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil3.png', 
+          imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto3.png',  
+          likeada: false, 
+          likers: 3
+        },
+        {
+          id: '4', 
+          nome: 'Gustavo Henrique', 
+          descricao: 'Isso sim que é TI!', 
+          imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil1.png', 
+          imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto4.png', 
+          likeada: false, 
+          likers: 1
+        },
+        {
+          id: '5', 
+          nome: 'Guilherme', 
+          descricao: 'Boa tarde galera do insta...', 
+          imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil2.png', 
+          imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto5.png',
+          likeada: false, 
+          likers: 32
+        }
+      ]
     };
   
-    this.mostrarValores = this.mostrarValores.bind(this);
-
   };
   
-  mostrarValores() {
-
-    if(this.state.nome === '' || this.state.idade === '')
-      alert('Preencha todos os campos');
-    else
-      alert(
-        'Conta criada com sucesso!!! \n\n' +
-        'Nome: ' + this.state.nome + '\n' +
-        'Idade: ' + this.state.idade + '\n' +
-        'Sexo: ' + this.state.sexos[this.state.sexo].sexoNome + ' \n' +
-        'Limite:  ' + this.state.limite.toFixed(0) + '\n' +
-        'Tipo de conta:  ' + ((this.state.isEstudante) ? 'Estudante' : 'Padrão')
-      );
-  }
-
   render(){
-    let sexosItem = this.state.sexos.map( (v, k) => {
-      return <Picker.Item key={k} value={k} label={v.sexoNome} />
-    })
-
     return(
 
       <View style={styles.container}>
-      
-        <Text style={styles.titulo}>Preencha todas as informações abaixo para criar sua conta. </Text>
-
-        <TextInput 
-        style={styles.inputText} 
-        onChangeText={ (valorNome) => this.setState({nome: valorNome}) }
-        placeholder="Informe seu nome" />
         
-        <TextInput 
-          style={styles.inputText} 
-          onChangeText={ (valorIdade) => this.setState({idade: valorIdade}) }
-          placeholder="Informe sua idade"
-        />
-
-        <Picker style={styles.picker}
-          selectedValue={this.state.sexo} 
-          onValueChange={(itemValue, itemIndex) => this.setState({sexo: itemValue})}>  
-            {sexosItem}
-        </Picker>
-      
-        <Slider
-          value={this.state.limite}
-          minimumValue={500}
-          maximumValue={1500}
-          onValueChange={ (valorLimite) => this.setState({limite: valorLimite}) }
-        />
-        <Text style={styles.inputSlider}>{this.state.limite.toFixed(0)}</Text>
+        <View style={styles.header}>
+          <TouchableOpacity>
+            <Image
+              source={require('./android/app/src/img/logo.png')}
+              style={styles.logo}
+            />
+          </TouchableOpacity>
         
-        <Text style={styles.inputs}>
-          Você é estudante? 
-          {(this.state.isEstudante) ? " Sim" : " Não"}
-          <Switch style={styles.switchEstudante}
-            value={this.state.isEstudante}
-            onValueChange={(valorSwitch) => this.setState({isEstudante: valorSwitch}) }
-          />
-        </Text>
-        
-        <Button style={styles.botao}
-          title='Abrir conta' 
-          onPress={this.mostrarValores}
-        />
+          <TouchableOpacity>
+            <Image 
+              source={require('./android/app/src/img/send.png')}
+              style={styles.send}
+            />
+          </TouchableOpacity>          
+        </View>
 
+      <FlatList 
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(item) => item.id}
+        data={this.state.feed}
+        renderItem={({item}) => <Lista data={item} />}
+      />
       </View>
     );
   }
@@ -97,31 +101,25 @@ class App extends Component{
 
 const styles = StyleSheet.create({
   container:{
-    flex:1,
-    margin: 20
+    flex:1
   },
-  titulo:{
-    fontSize: 23,
-    fontWeight: 'bold'
+  header:{
+    height: 55,
+    backgroundColor: '#FFF',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 5,
+    borderBottomWidth: 0.2,
+    shadowColor: '#000',
+    elevation: 1
   },
-  inputText:{
-    fontSize: 18,
-    marginBottom: 20,
-    borderBottomWidth: 1
+  logo:{
+
   },
-  inputSlider:{
-    fontSize: 18,
-    textAlign: 'center',
-  },
-  switchEstudante:{
-    textAlign: 'left'
-  },
-  picker:{
-    marginBottom: 10
-  },
-  inputs:{
-    fontSize: 18,
-    marginBottom: 20,
+  send:{
+    width: 23,
+    height: 23
   }
 });
 
