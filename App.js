ef83a4fbeff0147e66e7dd2770d44ada0868d4be
Robledo Castro <bedocastro@gmail.com>
2025-04-07 11:43:18 -0300

    Animated e interpolate

--- App.js
+++ App.js
@@ -2,27 +2,51 @@ import React, { useRef, useEffect } from 'react';
 import { View, Text, StyleSheet, Animated } from 'react-native';
 
 export default function App(){
-  const widthAnimada = useRef(new Animated.Value(150)).current;
-  const heightAnimada = useRef(new Animated.Value(50)).current;
+  const widthAnimada = useRef(new Animated.Value(0)).current;
+  const heightAnimada = useRef(new Animated.Value(5)).current;
+  const opacidadeAnimada = useRef(new Animated.Value(1)).current;
 
   useEffect(()=> {
-    Animated.timing(heightAnimada, {
-      toValue: 300,
-      duration: 2000,
-      useNativeDriver: false
-    }).start();
+    
+    Animated.sequence([
+      Animated.timing(widthAnimada, {
+        toValue: 100,
+        duration: 4000,
+        useNativeDriver: false
+      }),
+      Animated.timing(heightAnimada, {
+        toValue: 100,
+        duration: 4000,
+        useNativeDriver: false
+      })
+    ]).start( () => {
+      alert("Animação concluída!");
+    });
+
   }, []);
 
+  let porcentagemLargura = widthAnimada.interpolate({
+    inputRange: [0, 100],
+    outputRange: ['0%', '100%']
+  });
+
+  let porcentagemALtura = heightAnimada.interpolate({
+    inputRange: [5, 100],
+    outputRange: ['5%', '100%']
+  });
+
   return(
+
   
   <View style={styles.container}>
 
     <Animated.View
       style={{
-        width: widthAnimada,
-        height: heightAnimada,
+        width: porcentagemLargura,
+        height: porcentagemALtura,
         backgroundColor: '#4169e1',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        opacity: opacidadeAnimada
       }}
     >
       <Text style={styles.text}>Carregando...</Text>
